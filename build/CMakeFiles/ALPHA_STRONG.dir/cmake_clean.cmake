24c5e76f8816c49ca227747bead891fcfee13f1e
knowledge91 <dirkhornung91@gmail.com>
2018-04-04 17:16:14 +0200

    init

---
+++ build/CMakeFiles/ALPHA_STRONG.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ALPHA_STRONG.dir/main.cpp.o"
+  "ALPHA_STRONG.pdb"
+  "ALPHA_STRONG"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ALPHA_STRONG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ALPHA_STRONG.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ALPHA_STRONG.
+# This may be replaced when dependencies are built.
