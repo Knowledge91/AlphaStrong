07590b4b00333d30d262403b5757bf2dc64c19ac
knowledge91 <dirkhornung91@gmail.com>
2018-04-12 11:17:01 +0200

    Adaptive RK (Cash-Karp) implemented but only O(6) precise

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 #include <functional>
 #include <complex>
+#include <algorithm>
+#include <stdexcept>
 
 using namespace std;
 
@@ -13,12 +15,22 @@ public:
   }
 
   complex<double> run(complex<double> mu2, complex<double> mu1, complex<double> a1) {
-    uint stepCount = 10;
-    complex<double> h = (mu2-mu1)/(double)stepCount;
     function<complex<double>(complex<double>, complex<double>)> rhs = [&](complex<double> mu, complex<double> a) -> complex<double> {
       return -1.0/mu*betaFunction(a);
     };
-    return RungeKutta(mu1, a1, h, rhs, stepCount);
+    complex<double> h = (mu2-mu1)/1000.0;
+    complex<double> y = a1, t = mu1;
+    int i = 0;
+    while(t != mu2) {
+      i++;
+      // check if overstep
+      if (abs(mu2 - (t+h)) > abs(mu2 - t)) {
+        h = mu2 - t;
+      }
+      std::cout << i << std::endl;
+      RungeKutta(t, y, h, rhs);
+    }
+    return y;
   }
 
 
@@ -41,18 +53,53 @@ private:
     return beta_1*pow(a, 2) + beta_2*pow(a, 3) + beta_3*pow(a, 4) + beta_4*pow(a, 5) + beta_5*pow(a, 6);
   };
 
-  complex<double> RungeKutta(complex<double> t, complex<double> y, const complex<double> &h, const function<complex<double>(complex<double>, complex<double>)> &f, const uint &n) {
-    complex<double> k1, k2, k3, k4;
-    for (uint i = 1; i <= n; i++) {
-      k1 = f(t, y);
-      k2 = f(t + h/2.0, y + h*k1/2.0);
-      k3 = f(t + h/2.0, y + h*k2/2.0);
-      k4 = f(t + h, y + h*k3);
-      t = t + h;
-      y = y + h/6.0*(k1 + 2.0*k2 + 2.0*k3 + k4);
-    }
+  // Adaptive Explicit Runge-Kutta 4th order (Cash-Karp method)
+  void RungeKutta(complex<double> &t, complex<double> &y, complex<double> &h, const function<complex<double>(complex<double>, complex<double>)> &f) {
+    double eps = 1e-12;
+    complex<double> k1, k2, k3, k4, k5, k6;
+    complex<double> yError = 0., yTemp;
+    complex<double> hTemp = h;
 
-    return y;
+    // Cash-Karp coefficients
+    double c2 = 1.0/5.0, c3 = 3.0/10.0, c4 = 3.0/5.0, c5 = 1.0, c6 = 7.0/8.0;
+    double b1 = 37.0/378.0, b2 = 0, b3 = 250.0/621.0, b4 = 125.0/594.0, b5 = 0.0, b6 = 512.0/1771.0;
+    double B1 = 2825.0/27648.0, B2 = 0.0, B3 = 18575.0/48384.0, B4 = 13525.0/55296.0, B5 = 277.0/14336.0, B6 = 1.0/4.0;
+    double a21 = 1.0/5.0;
+    double a31 = 3.0/40.0, a32 = 9.0/40.0;
+    double a41 = 3.0/10.0, a42 = -9.0/10.0, a43 = 6.0/5.0;
+    double a51 = -11.0/54.0, a52 = 5.0/2.0, a53 = -70.0/27.0, a54 = 35.0/27.0;
+    double a61 = 1631.0/55296.0, a62 = 175.0/512.0, a63 = 575.0/13824.0, a64 = 44275.0/110592.0, a65 = 253.0/4096.0;
+
+    while(true) {
+      k1 = f(t, y);
+      k2 = f(t + c2*hTemp, y + hTemp*(a21*k1));
+      k3 = f(t + c3*hTemp, y + hTemp*(a31*k1 + a32*k2));
+      k4 = f(t + c4*hTemp, y + hTemp*(a41*k1 + a42*k2 + a43*k3));
+      k5 = f(t + c5*hTemp, y + hTemp*(a51*k1 + a52*k2 + a53*k3 + a54*k4));
+      k6 = f(t + c6*hTemp, y + hTemp*(a61*k1 + a62*k2 + a63*k4 + a64*k4 + a65*k5));
+
+      yTemp = y + hTemp*(b1*k1 + b2*k2 + b3*k3 + b4*k4 + b5*k5 + b6*k6);
+      yError = (b1-B1)*k1 + (b2-B2)*k2 + (b3-B3)*k3 + (b4-B4)*k4 + (b5-B5)*k5 + (b6-B6)*k6;
+
+      double err = abs(yError)/eps;
+      if(err < 1) {
+        t = t + hTemp;
+        y = yTemp;
+        if(err>1.89e-4) {
+          h = 0.9*hTemp*pow(abs(yError),-0.25);
+        } else {
+          h = 5.0*hTemp;
+        }
+        break;
+      } else {
+        hTemp = 0.1*hTemp;
+        std::cout << "hTemp, t \t" << hTemp << "\t" << t << "\t" << yError << std::endl;
+        if ( t+hTemp == t ) {
+          throw std::runtime_error("RK: stepsize too small");
+        }
+        continue;
+      }
+    }
   }
 
   uint nf_;
@@ -75,7 +122,7 @@ int main() {
   std::cout << std::setprecision(17);
   complex<double> a1(0.101627, 0.0);
   complex<double> mu1(3.1570893124000001, 0.0);
-  complex<double> mu2(3.0, 0.0);
+  complex<double> mu2(3.2, 0.0);
 
   RunAlphaStrong runner;
   std::cout << runner.run(mu2, mu1, a1) << std::endl;
